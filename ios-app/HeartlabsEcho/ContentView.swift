7ae861a1910fd09bb21e9fa7d49d79e5008a8509
heartlabs <neidhart.orlich@gmail.com>
2026-07-11 15:01:19 +0200

    tree redesign

--- ios-app/HeartlabsEcho/ContentView.swift
+++ ios-app/HeartlabsEcho/ContentView.swift
@@ -62,7 +62,7 @@ struct ContentView: View {
                 }
             )
             .tabItem {
-                Label("Me", systemImage: "figure.mind.and.body")
+                Label("Profile", systemImage: "person")
             }
                 .tag(2)
 
